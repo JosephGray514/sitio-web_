189d056468e8aa4750b6cc0b5b5abac2f1e6c1c8
JosephGray514 <josephgray514@gmail.com>
2023-11-28 11:42:13 -0600

    prueba con el git add --patch

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'))
 
+app.get('/hola', (req, res) => {
+    res.send('ruta Hola')
+})
+
+app.get('/adios', (req, res) => {
+    res.send('ruta Adios')
+})
+
 app.listen(3000, ()=> {
     console.log('Example app listening on port 3000!')
     console.log('status')
 })
+
